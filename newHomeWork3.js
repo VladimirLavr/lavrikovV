268c4807820100152c9910ecb53ff083e4fc52e9
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-03-23 18:35:12 +0200

    Update newHomeWork3.js

--- newHomeWork3.js
+++ newHomeWork3.js
@@ -1,33 +1,27 @@
+const calc = prompt('Какое количество операций хотите произвести на калькуляторе?', "");
 
+for (let i = 0; i < calc; i++) {
+    const firstNumber = +prompt('Пожалуйста введите первое число', ""),
+        secondNumber = +prompt('Пожалуйста введите второе число', ""),
+        operator = prompt('какое действие с числами нужно совершить?', "");
 
-
-const a = prompt('Какое количество операций хотите произвести на калькуляторе?',"")
-for (let i=0; i<a;i++) {
-    const b = +prompt('Пожалуйста введите первое число', ""),
-        c = +prompt('Пожалуйста введите второе число', ""),
-        d = prompt('какое действие с числами нужно совершить?', "");
-
-
-
-        let result=console.log;
-        switch (d){
-            case "-":
-                result(b-c);
+    switch (operator) {
+        case "-":
+            console.log(firstNumber - secondNumber);
             break;
-            case "+":
-                result(b+c);
-                break;
-            case "*":
-                result(b*c);
-                break;
-            case "/":
-                result(b/c);
-                break;
-                default : (`${d}==null || ${d}===''`) ? alert(`Мы не можем выполнить действие с оператором: ${d}`) : null;
-
-        }
-
+        case "+":
+            console.log(firstNumber + secondNumber);
+            break;
+        case "*":
+            console.log(firstNumber * secondNumber);
+            break;
+        case "/":
+            console.log(firstNumber / secondNumber);
+            break;
+        default:
+            alert(`Мы не можем выполнить действие с оператором: ${operator}`);
     }
+}
 
 
 
