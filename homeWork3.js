f27c54eb4eadf36137188a333d7b9c06dad1d07a
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-03-23 15:58:15 +0200

    Add files via upload

---
+++ homeWork3.js
@@ -0,0 +1,31 @@
+
+
+
+let a = prompt('Какое количество операций хотите произвести на калькуляторе?',"")
+for (let i=0; i<a;i++) {
+    const b = +prompt('Пожалуйста введите первое число', ""),
+        c = +prompt('Пожалуйста введите второе число', ""),
+        d = prompt('какое действие с числами нужно совершить?', "");
+    function operator(b,c,d){
+        let result=console.log;
+        if(d=='-'){
+            result(b-c)
+        }else if (d=='+'){
+            result(b+c)
+        }else if (d=='*'){
+            result(b*c)
+        }else if (d=='/'){
+            result(b/c)
+        }else if (`${d}==null || ${d}===''`) alert(`Мы не можем выполнить действие с оператором: ${d}`)
+    }
+    operator(b,c,d)
+}
+
+
+
+
+
+
+
+
+
