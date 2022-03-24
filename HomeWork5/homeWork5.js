60d2a1784f3a0e3a6ff6d5a63dce231819670a58
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-03-24 15:11:57 +0200

    Update homeWork5.js

--- HomeWork5/homeWork5.js
+++ HomeWork5/homeWork5.js
@@ -1,21 +1,32 @@
 const movies = [
-    {id: 1, name: "Fast and Furious", cost: 5.99},
-    {id: 2, name: "Fast and Furious 2", cost: 3.99},
-    {id: 3, name: "Fast and Furious 4", cost: 4.99},
-    {id: 4, name: "Shrek", cost: 12.99},
+    { id: 1, name: "Fast and Furious", cost: 5.99 },
+    { id: 2, name: "Fast and Furious 2", cost: 3.99 },
+    { id: 3, name: "Fast and Furious 4", cost: 4.99 },
+    { id: 4, name: "Shrek", cost: 12.99 },
 ];
 
 const cart = [];
 
-
 function addToCart(id) {
-    // your solution here...
-}
+    movies.filter(addMovie => {
+        if (id === addMovie.id) {
+            return cart.push(addMovie)
+        }
+    })
 
-function printCheck() {
-    // your solution here...
 }
 
 addToCart(2);
-addToCart(4);
+
+function printCheck() {
+    const newCart = cart.map(movieItem => {
+        return (`${movieItem.id} -- ${movieItem.name} -- $${movieItem.cost}`)
+    })
+    console.log(newCart)
+}
+
 printCheck();
+
+
+
+
