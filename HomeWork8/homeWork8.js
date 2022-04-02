865e8b11424fed85dae3bee0d3303d1b2afb7940
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-04-02 19:35:12 +0300

    Update homeWork8.js

--- HomeWork8/homeWork8.js
+++ HomeWork8/homeWork8.js
@@ -5,26 +5,28 @@ let arr=[];
 alert("Введите цвет телефона в поле, а также выберите с доступных");
 
 const phone = () => {
+let option=doc.options[doc.selectedIndex].text;
 
     let cart = {};
 
-    if (document.querySelector('.inp').value === doc.options[doc.selectedIndex].text) {
-        const model = prompt('Какой модель телефона вас интересует?', '')
-        const memorySize = prompt('Какой обьем памяти телефона вас интересует?', '')
-        const hddSize = prompt('Какое количество оперативной памяти телефона вас интересует?', '')
-        const osVersion = prompt('Какая операционна система телефона вас интересует?', '')
-        const screen = prompt('Какой типа экрана?', '')
-        cart.color = doc.options[doc.selectedIndex].text;
+    if (document.querySelector('.inp').value === option) {
+        const model = prompt('Какой модель телефона вас интересует?', '');
+        const memorySize = prompt('Какой обьем памяти телефона вас интересует?', '');
+        const hddSize = prompt('Какое количество оперативной памяти телефона вас интересует?', '');
+        const osVersion = prompt('Какая операционна система телефона вас интересует?', '');
+        const screen = prompt('Какой типа экрана?', '');
+        cart.color = option;
         cart.model = model;
         cart.memorySize = memorySize;
         cart.hddSize = hddSize;
         cart.osVersion = osVersion;
         cart.screen = screen;
-        return cart && arr.push(cart) && console.log(arr);
-
+        arr.push(cart) && console.log(arr);
+        return arr;
     } else {
         alert('Вы ничего не выбрали');
     }
 
 };
 
+ 
