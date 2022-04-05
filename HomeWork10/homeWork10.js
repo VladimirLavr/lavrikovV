ac9c1b7f13fdb7992a427e5648a8dd05f70745d0
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-04-05 14:46:06 +0300

    Update homeWork10.js

--- HomeWork10/homeWork10.js
+++ HomeWork10/homeWork10.js
@@ -2,25 +2,29 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function chunk(arr, chunkSize) {
 
-    let result = [];
+    let newArr = [];
 
-    if (chunkSize > 1) {
-        for (let i = 0; i < Math.ceil(arr.length / chunkSize); i++) {
-            result.push(arr.slice((i * chunkSize), (i * chunkSize) + chunkSize));
+    if (chunkSize >= 1) {
+        for (let i = 0; i < arr.length; i += chunkSize) {
+            newArr.push(arr.slice(i, i + chunkSize));
         }
-        return result;
+        return newArr;
 
     } if (chunkSize === 1) {
         return arr;
 
     } else return 'Пользователь, ты не прав!';
-};
+}
 
-const result = (chunk(arr, 1));  
 
+const result = chunk(arr, 0);
 console.log(result);
 
 
- 
+
+
+
+
+
 
 
