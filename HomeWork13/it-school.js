742ae87811d3591c7d1df01c85b69452d5472fa8
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-04-17 09:15:25 +0300

    Add files via upload

---
+++ HomeWork13/it-school.js
@@ -0,0 +1,53 @@
+class ITSchool {
+    constructor(name, description, maxGroupCount, maxStudentsCountPerGroup) {
+        this.name = name;
+        this.description = description;
+        this.maxGroupCount = maxGroupCount;
+        this.maxStudentsCountPerGroup = maxStudentsCountPerGroup;
+        this.availableCourses = ["Front-end Pro", "Front-end Basic", "Java Basic"];
+        this.startedGroups = [];
+    }
+
+
+    registerCourse(courseName, totalLessons, availableTeachersAmount) {
+        if (!(this.availableCourses.includes(courseName))) {
+            this.startedGroups.push(new Course(courseName, totalLessons, availableTeachersAmount));
+
+        } else {
+            console.log(`Извините,группа ${courseName} уже существует!`);
+        }
+    }
+
+
+    startLearningGroup(courseName, teacherName, amountOfStudents) {
+        if (this.availableCourses.includes(courseName)) {
+            if (teacherName !== "") {
+                amountOfStudents -= 1;
+                this.startedGroups.push(new LearningGroup(courseName, teacherName, amountOfStudents));
+
+            } else console.log(`Извините,нет преподавателя!`);
+
+        } else console.log(`Извините,курса ${courseName} не существует!`);
+    }
+
+
+    endLearningGroup(courseName, teacherName) {
+        if (this.startedGroups.some((item) => item.teacherName === teacherName)) {
+            if (this.startedGroups.some((course) => course.courseName === courseName)) {
+                this.startedGroups = this.startedGroups.filter((teacher) => teacher.teacherName !== teacherName);
+
+            } else console.log(`Извините, курса ${courseName} не существует`);
+
+        } else console.log(`Извините, преподавателя ${teacherName} не существует`);
+    }
+
+
+    getLearningGroups(courseName) {
+        if (this.startedGroups.some((item) => item.courseName === courseName)) {
+            this.startedGroups = this.startedGroups.filter((item) => item.courseName === courseName);
+            return this.startedGroups;
+
+        } else console.log(`Извините, ничего по вашему запросу не найдено!`);
+    }
+}
+
---
+++ HomeWork13/learning-group.js
@@ -0,0 +1,19 @@
+class LearningGroup {
+    constructor(courseName, teacherName, amountOfStudents) {
+        this.courseName = courseName;
+        this.teacherName = teacherName;
+        this.amountOfStudents = amountOfStudents;
+        this.passedLessons = [];
+    }
+
+
+    doneLesson(title, topics) {
+        if (itSchool.name !== title) {
+            if (itSchool.description !== topics) {
+                itSchool.startedGroups.forEach(item => item.passedLessons.push(new Lesson(title, topics)));
+
+            } if (itSchool.name === title) console.log('Такая тема уже существует');
+
+        } if (itSchool.name === title) console.log('Такай урок уже существует');
+    }
+}
